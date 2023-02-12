74da0208f798b2c00f0f1235254f447bab619728
M1k0 <44602955+mihirverma1@users.noreply.github.com>
2023-02-12 20:55:19 +0530

    domain and direnum

--- Scanner.py
+++ Scanner.py
@@ -1,4 +1,5 @@
 import os;
-
 ip= input('Enter the IP to scan \n')
-os.system('nmap -T5 -A -v -p- -oN nmapscan_'+ ip)
+os.system('nmap -T5 -A -v -p- -oN nmapscan_'+ ip);
+os.system("gobuster dns -w /usr/share/amass/wordlists/subdomains-top1mil-20000.txt -t 200 -o domainenum.txt -d" + ip);
+os.system("gobuster dir -w /usr/share/dirbuster/wordlists/directory-list-2.3-medium.txt -t 200 -o direnum.txt -u" + ip);
