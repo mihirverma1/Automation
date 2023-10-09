321e8d4ab4b52704b4d843810c78fed1964bef66
M1k0 <mihiratwork@gmail.com>
2023-10-09 20:30:32 +0530

    Update Scanner.py
    Change in nmap script

--- Scanner.py
+++ Scanner.py
@@ -1,5 +1,5 @@
 import os;
 ip= input('Enter the IP to scan \n')
-os.system('nmap -T4 -sV -A -v '+ ip);
+os.system('nmap -sS -sC -sV -oA nmap -v '+ ip);
 os.system("gobuster vhost -w /usr/share/SecLists/Discovery/DNS/subdomains-top1million-5000.txt -t 50 --append-domain -u" + ip);
 os.system("gobuster dir -w /usr/share/dirbuster/wordlists/directory-list-2.3-medium.txt -t 50 -o direnum.txt -u" + ip);
